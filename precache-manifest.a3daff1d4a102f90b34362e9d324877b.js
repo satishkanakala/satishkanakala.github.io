edf394f01335df364b3d92e9400d70e7b22bbe5c
satishkanakala <satishkumarkanakala@gmail.com>
2020-05-08 11:59:41 +0530

    React covid app

---
+++ precache-manifest.a3daff1d4a102f90b34362e9d324877b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b0f2b0770b5e5fea6f450fc93bfa6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "187e53035292a052ad73",
+    "url": "/static/css/main.f2c544c4.chunk.css"
+  },
+  {
+    "revision": "0714b8b1af724dffa656",
+    "url": "/static/js/2.411a1129.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.411a1129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187e53035292a052ad73",
+    "url": "/static/js/main.d7c32ff2.chunk.js"
+  },
+  {
+    "revision": "e150e6475111825e9e8c",
+    "url": "/static/js/runtime-main.ade9ce96.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid.d7265326.png"
+  }
+]);
